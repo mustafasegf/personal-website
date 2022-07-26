67e84a80bed61906cfba807f387123ee777294e5
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2022-07-26 23:02:07 +0700

    feat: added simple layout

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+export default function Counter() {
+
+  return (
+    <>
+      <p className="text-justify">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec convallis varius tincidunt. Etiam id ipsum a nunc tincidunt volutpat lacinia sit amet velit. Nullam euismod arcu eu lobortis blandit. Pellentesque quis dui tincidunt, imperdiet enim a, pellentesque sapien. Fusce dapibus molestie ultrices. Pellentesque tempus viverra facilisis. Praesent tincidunt, lacus vitae feugiat eleifend, neque quam varius ipsum, lobortis tempus erat velit eu massa. Duis eu felis id ligula lacinia ornare eu vitae felis.
+
+Nulla tincidunt sed dolor vel vulputate. Pellentesque eu pulvinar magna. In vulputate scelerisque fermentum. Quisque tincidunt sodales felis eget sagittis. Nullam pulvinar risus vitae magna tincidunt, et finibus nisl egestas. Praesent quis sapien nulla. Proin mattis augue vitae nisl interdum accumsan. Donec interdum a orci ac fermentum. Maecenas ornare augue vitae mi aliquet placerat. Morbi porttitor est eu consectetur posuere. Etiam id ex odio. Curabitur non eleifend leo, sit amet faucibus ipsum. Nullam faucibus diam vitae interdum consequat. Maecenas finibus condimentum accumsan. Vestibulum viverra sit amet erat id maximus. Nunc dignissim ligula faucibus dolor pellentesque, eu commodo elit pellentesque.
+      </p>
+    </>
+  );
+}
