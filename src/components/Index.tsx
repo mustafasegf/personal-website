53025fe2a1c525a92dcf2dffafab1f48aca28fb0
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2022-07-28 13:59:34 +0700

    feat: added about me

---
+++ src/components/Index.tsx
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
+
+export const Accent: React.FC<{ children: string }> = ({ children }) => (
+  <span className="text-lg text-accent whitespace-nowrap"> {children}</span>
+)
+
+export const AnimatedText: React.FC<{ words: string[] }> = ({ words }) => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((index + 1) % words.length);
+    }, 1500);
+
+    return () => clearInterval(interval);
+  }, [index]);
+
+  const sentence = {
+    hidden: {
+      opacity: 1,
+      transition: {
+        delay: 0.5,
+      },
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        delay: 0.5,
+        staggerChildren: 0.025,
+      },
+    },
+  };
+
+  const letter = {
+    hidden: {
+      opacity: 0,
+      y: 50,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+    },
+  };
+
+  return (
+
+    <motion.span
+      initial="hidden"
+      animate="visible"
+      className="text-accent"
+      variants={sentence}
+      key={words[index]}
+    >
+      {
+        words[index].split('').map((char, i) => {
+          return (
+            <motion.span key={words[index] + '-' + i} variants={letter}>
+              {char}
+            </motion.span>
+          );
+        })
+      }
+    </motion.span>
+  )
+}
