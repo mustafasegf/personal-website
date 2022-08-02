61ee8b518e19ed906e2727dfc06c2ffabe55f443
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2022-08-03 02:22:15 +0700

    feat: added meta tags

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,6 +5,11 @@ interface ImportMetaEnv {
 	readonly PHONE: string;
 	readonly LINKEDIN: string;
 	readonly GITHUB: string;
+  readonly TWITTER: string;
+  readonly INSTAGRAM: string;
+  readonly YOUTUBE: string;
+  readonly FACEBOOK: string;
+  readonly DOMAIN: string;
 }
 
 interface ImportMeta {
